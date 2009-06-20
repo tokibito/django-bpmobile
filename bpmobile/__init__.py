67d7d6900342e16953079cc84322c528c8afd2bb
Shinya Okano <xxshss@yahoo.co.jp>
2009-06-20 20:38:57 +0900

    remove patched runserver command, and added handler patch when __init__.py is loaded.

--- bpmobile/__init__.py
+++ bpmobile/__init__.py
@@ -1,1 +1,6 @@
 __version__ = '0.1'
+
+from django.core.handlers.wsgi import WSGIHandler
+from bpmobile.wsgi import DetectEncodingWSGIRequest
+
+WSGIHandler.request_class = DetectEncodingWSGIRequest
